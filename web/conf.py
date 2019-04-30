cc1f8528d924474644650beb7f23b766bc4c84bb
Dominik Stańczak <stanczakdominik@gmail.com>
2019-04-30 20:38:06 +0200

    Update conf.py

--- web/conf.py
+++ web/conf.py
@@ -33,6 +33,7 @@ RIOT = "https://riot.im/app/#/room/#plasmapy:openastronomy.org"
 TWITTER = "https://twitter.com/plasmapy"
 MAILING_LIST = "https://groups.google.com/forum/#!forum/plasmapy"
 TELECONFERENCE_CALENDAR = "https://calendar.google.com/calendar?cid=bzVsb3ZkcW0zaWxsam00ZTlrMDd2cmw5bWdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+TELECON_NOTES = "https://drive.google.com/drive/folders/0ByPG8nie6fTPV1FQUEkzMTgtRTg?usp=sharing"
 RITIEK = "https://ritiek.github.io/posts/"
 
 # Nikola is multilingual!
@@ -157,6 +158,7 @@ NAVIGATION_LINKS = {
           (TWITTER, "Twitter"),
           (MAILING_LIST, "Mailing list"),
           (TELECONFERENCE_CALENDAR, "Teleconference calendar"),
+          (TELECON_NOTES, "Teleconference notes"),
         ), "Contact us!"),
         # ("/team", "Team"),
         ((
