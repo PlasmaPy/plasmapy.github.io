a8af1a18abf4afa3666ba8b27a195eb65888ce15
Dominik Stańczak <stanczakdominik@gmail.com>
2020-02-28 18:14:32 +0100

    GNU Terry Pratchett

--- web/conf.py
+++ web/conf.py
@@ -1261,7 +1261,9 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = r"""
+<meta http-equiv="X-Clacks-Overhead" content="GNU Terry Pratchett" />
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
