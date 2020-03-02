c9aa4bc2db00c06a360d6c0e60707c5506e2a73f
Erik Everson <eteverson@gmail.com>
2020-03-01 19:52:24 -0800

    add back extension markdown.extensions.meta

--- web/conf.py
+++ web/conf.py
@@ -1123,7 +1123,10 @@ PRETTY_URLS = True
 #       with the MarkdownExtension class and should not be added here.
 # Defaults are markdown.extensions.(fenced_code|codehilite|extra)
 # markdown.extensions.meta is required for Markdown metadata.
-MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.codehilite', 'markdown.extensions.extra']
+MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code',
+                       'markdown.extensions.codehilite',
+                       'markdown.extensions.extra',
+                       'markdown.extensions.meta']
 
 # Options to be passed to markdown extensions (See https://python-markdown.github.io/reference/)
 # Default is {} (no config at all)
