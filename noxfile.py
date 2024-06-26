4af67a8f58256546a3e38da646bca733fcf851e7
Nick Murphy <namurphy@cfa.harvard.edu>
2024-06-26 17:40:30 -0400

    Update Nox sessions

--- noxfile.py
+++ noxfile.py
@@ -5,17 +5,26 @@ nox.options.default_venv_backend = "uv|virtualenv"
 
 @nox.session
 def build(session) -> None:
+    """Build the site with Nikola."""
     session.install("-r", "requirements.txt")
     session.run("nikola", "build")
 
 
 @nox.session
 def check(session) -> None:
+    """Check links and files in the generated site."""
     session.install("-r", "requirements.txt")
+    session.run("nikola", "build")
     session.run(
         "nikola",
         "check",
         "--check-links",
-        #        "--check-files",
         "--find-sources",
     )
+
+
+@nox.session
+def clean(session) -> None:
+    """Remove output files."""
+    session.install("-r", "requirements.txt")
+    session.run("nikola", "clean")
