d6dff1b35b3307287ab8d5749fbb74a28a0bac2e
Nick Murphy <namurphy@cfa.harvard.edu>
2026-01-12 13:57:17 -0500

    Change a Zoom link

--- conf.py
+++ conf.py
@@ -35,7 +35,6 @@ PROJECT_REPOSITORY = "https://github.com/PlasmaPy/PlasmaPy"
 CHAT = "https://app.element.io/#/room/#plasmapy:openastronomy.org"
 TELECON_CALENDAR = "https://calendar.google.com/calendar/embed?src=c_sqqq390s24jjfjp3q86pv41pi8%40group.calendar.google.com&ctz=America%2FNew_York"
 YOUTUBE_CHANNEL = "https://www.youtube.com/channel/UCSH6qzslhqIZKTAJmHPxIxw"
-JITSI = "https://meet.jit.si/plasmapy"
 ZOOM = "https://zoom.us/j/91633383503?pwd=QWNkdHpWeFhrYW1vQy91ODNTVG5Ndz09"
 
 # Nikola is multilingual!
